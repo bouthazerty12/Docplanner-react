3dc81086155fd2aa5958fd59d7c00f7d76975fda
bothazerty12 <bouthaynaakrouti522@gmail.com>
2019-10-17 13:02:09 +0200

    Updates

---
+++ precache-manifest.249f87e984c6c9071a23c65bde69543d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8639fcc12010efa795f08592903bb960",
+    "url": "/Docplanner-react/index.html"
+  },
+  {
+    "revision": "b030c43f022d8e171830",
+    "url": "/Docplanner-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e736dde83d67b9265a6b",
+    "url": "/Docplanner-react/static/css/main.f44192ad.chunk.css"
+  },
+  {
+    "revision": "b030c43f022d8e171830",
+    "url": "/Docplanner-react/static/js/2.6b48e913.chunk.js"
+  },
+  {
+    "revision": "e736dde83d67b9265a6b",
+    "url": "/Docplanner-react/static/js/main.d4033b5b.chunk.js"
+  },
+  {
+    "revision": "09fd3255b360e4af816d",
+    "url": "/Docplanner-react/static/js/runtime-main.51342bcc.js"
+  }
+]);
